1302df0a0149024738001912077e235a85ca05aa
¨LisMCee¨ <lisacanbefound@gmail.com>
2021-07-21 16:40:32 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An evolving project testing machine learning algorithms to predict severity of COVID (all time deaths per 100,000 population) per country',
+    author='Lisa Crowther',
+    license='MIT',
+)
